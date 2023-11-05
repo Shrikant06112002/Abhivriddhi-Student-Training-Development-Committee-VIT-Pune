a1736584223c92e7b7d485f613eed9093d0b481c
Shrikant06112002 <100031999+Shrikant06112002@users.noreply.github.com>
2023-11-05 12:53:02 +0530

    Your commit message here

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,10 +204,10 @@ document.addEventListener("DOMContentLoaded", () => {
    * Clients Slider
    */
   new Swiper(".clients-slider", {
-    speed: 400,
+    speed: 100000,
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 0,
       disableOnInteraction: false,
     },
     slidesPerView: "auto",
@@ -239,56 +239,56 @@ document.addEventListener("DOMContentLoaded", () => {
   /**
    * Init swiper slider with 1 slide at once in desktop view
    */
-  new Swiper(".slides-1", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  // new Swiper(".slides-1", {
+  //   speed: 600,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false,
+  //   },
+  //   slidesPerView: "auto",
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "bullets",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  // });
 
-  /**
-   * Init swiper slider with 3 slides at once in desktop view
-   */
-  new Swiper(".slides-3", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40,
-      },
+  // /**
+  //  * Init swiper slider with 3 slides at once in desktop view
+  //  */
+  // new Swiper(".slides-3", {
+  //   speed: 600,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false,
+  //   },
+  //   slidesPerView: "auto",
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "bullets",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  //   breakpoints: {
+  //     320: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 40,
+  //     },
 
-      1200: {
-        slidesPerView: 3,
-      },
-    },
-  });
+  //     1200: {
+  //       slidesPerView: 3,
+  //     },
+  //   },
+  // });
 
   /**
    * Porfolio isotope and filter
